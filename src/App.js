aa9948fccfcf565837e4851e5da55da060fcd96a
long <bekeo0302@gmail.com>
2021-12-29 16:53:49 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,532 @@
+import "./App.css";
+import logo from "./images/logo.png";
+import mainBanner from "./images/mainBanner.png";
+import introductionBg from "./images/introductionBg.png";
+import besBg from "./images/besBg.png";
+import bestimg from "./images/bestimg.png";
+import bestImage02 from "./images/bestImage02.png";
+import bestImage03 from "./images/bestImage03.png";
+import producticonBg from "./images/producticonBg.png";
+import img01 from "./images/img01.png";
+import icon from "./images/icon.png";
+import object01 from './images/object01.png'
+import obj02 from './images/obj02.png'
+import object03 from './images/object03.png'
+import img from './images/img.png'
+import xucxac from './images/xucxac.png'
+import xucxac02 from './images/xucxac02.png'
+import newsImg01 from './images/newsImg01.png'
+import newsImg03 from './images/newsImg03.png'
+import hot from './images/hot.png'
+import video from './images/video.png'
+import videobox from './images/videobox.png'
+import objvideo from './images/objvideo.png'
+import nguoi from './images/nguoi.png'
+import videoObject07 from './images/videoObject07.png'
+import tim from './images/tim.png'
+import videoObject03 from './images/videoObject03.png'
+import error from './images/error.png'
+import n from './images/n.png'
+
+
+const productList = [
+  {
+    id: 1,
+    imgSrc: bestimg,
+    description: 'How to create mobile-optimized'
+  },
+  {
+    id: 2,
+    imgSrc: bestImage02,
+    description: 'How to create mobile-optimized'
+  },
+  {
+    id: 3,
+    imgSrc: bestImage03,
+    description: 'How to create mobile-optimized'
+  },
+]
+const menu = [
+  {
+    id: 1,
+    text: "Introduction"
+  },
+  {
+    id: 2,
+    text: "solution"
+  },
+  {
+    id: 3,
+    text: "Rate plan"
+  },
+  {
+    id: 4,
+    text: "login"
+  },
+  {
+    id: 5,
+    text: "Apply for free use"
+  }
+]
+const productItem = [
+
+  {
+    id: 1,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+  },
+  {
+    id: 2,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 3,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 4,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 5,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 6,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 7,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 8,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 9,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 10,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 11,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 12,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 13,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 14,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 15,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 16,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 17,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 18,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 19,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 20,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 21,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 22,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 23,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 24,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+  {
+    id: 25,
+    imgSrc: img01,
+    description: '[what happen] How to create',
+    price: "2,500 won",
+    likes: 236,
+    icon: icon
+
+  },
+
+]
+function App() {
+  return (
+    <div className="App">
+      <header className="header-container">
+        <img src={logo} alt="" />
+        <nav>
+          <ul className="menu">
+            {menu.map(item => (
+              <li key={item.id} className="menu-item">{item.text}</li>
+            ))}
+
+          </ul>
+        </nav>
+      </header>
+      <div className="box-container">
+        <img src={mainBanner} />
+        <img src={introductionBg} />
+        <p className="box-title">WHAT HAPPENDED!</p>
+        <p className="box-content">
+          How to create mobile-optimized videos in minutes. Not a designer,
+
+          every team makes a lot of videos Can be trimmed. Take the first{" "}
+
+          step to your brand's success. How to create
+          mobile-optimized videos in minutes.
+        </p>
+      </div>
+      <div className="product-section" style={{
+        backgroundImage: "url('" + besBg + "')",
+      }}>
+        <h2 className="product__title">BEST PRODUCT</h2>
+        <p className="product__paragraph">How to create mobile-optimized videos in minutes. Not a designer,
+          every team makes a lot of videos Can be trimmed. Take the first </p>
+        <div className="product__list"  >
+          {productList.map(product => (
+            <div key={product.id} className="product-item">
+              <div style={{
+                backgroundImage: "url('" + producticonBg + "')",
+              }} className="product-item__number">0{product.id}</div>
+              <img src={product.imgSrc} />
+              <div className="product-item__description">
+                <span>{product.description}</span>
+                <p></p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="product-container">
+        {
+          productItem.map(item => (
+            <div key={item.id} className="product-box">
+              <img className="product-box-img" src={item.imgSrc} alt="" />
+              <p>{item.description} </p>
+              <div className="product-box-description">
+                <p>{item.price}</p>
+                <p className="product-likes"> <img className="icon" src={item.icon} alt="" /> {item.price}</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+      <div className="see-more">
+        <button className="btn-see">
+          see more
+        </button >
+      </div>
+      <div className="brand-container">
+        <p className="brand-title">BRAND STORY</p>
+        <p className="brand-content">
+          모바일에 최적화된 비디오를 단 몇 분 만에 제작해보세요. 디자이너가 아니더라도,  모든 팀에서
+          다듬을 수 있습니다. 브랜드의 성공을 위한 첫 걸음을  내딛어보세요. 비디오를 단 몇 분 만에 제작해보세요.</p>
+        <img className="brand-obj" src={object01} alt="" />
+        <img className="brand-obj-two" src={obj02} alt="" />
+        <img className="brand-obj-three" src={object03} alt="" />
+        <div className="brand-box">
+          <div className="brand-box-top"></div>
+          <div className="brand-box-bottom">
+            <img src={img} alt="" />
+            <div className="brand-box-text">
+              <p className="brand-box-title">What Happened’s Brand story</p>
+              <p className="brand-box-content">
+                청춘이 있는 바이며, 말이다. 같지 노년에게서 어디 모래뿐일 무엇을 풀이 옷을
+                봄바람이다. 새 천고에 놀이 내는 찾아 창공에 광야에서 살았으며, 듣는다. 수 있는
+                긴지라 사는가 낙원을 웅대한 아니다. 내는 이상 할지라도 피다. 피부가 불러 피고
+                인간의 타오르고 이성은 끝까지 칼이다. 쓸쓸한 희망의 못할 것은 목숨을 수 그들을
+                아름답고 고행을 끝에 설산에서 황금시대를 이상을 운다. </p>
+              <button className="brand-btn">
+                SEE MORE
+              </button>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      <div className="happend">
+        <span className="happend-title">Happened’s issue</span>
+        <p className="happend-content">
+          모바일에 최적화된 비디오를 단 몇 분 만에 제작해보세요. 디자이너가 아니더라도,  모든 팀에서
+          다듬을 수 있습니다. 브랜드의 성공을 위한 첫 걸음을  내딛어보세요. 비디오를 단 몇 분 만에 제작해보세요.
+        </p>
+        <img className="dice" src={xucxac} alt="" />
+        <img className="dice-two" src={xucxac02} alt="" />
+        <button className="brand-gold">
+          SEE MORE
+        </button>
+        <div className="list-happend">
+          <div className="list-happend-left">
+            <p className="whpn-issue">
+              whpn
+              issue
+            </p>
+            <img className="whpn-issue-img" src={newsImg01} alt="" />
+          </div>
+          <div className="list-happend-right">
+            <img className="hot" src={hot} alt="" />
+            <div className="list-happend-item">
+              <span>B BRAND</span>
+              <img src={newsImg03} alt="" />
+            </div>
+            <div className="list-happend-item">
+              <span>B BRAND</span>
+              <img src={newsImg03} alt="" />
+            </div>
+            <div className="list-happend-item">
+              <span className="acvive">B BRAND</span>
+              <img src={newsImg03} alt="" />
+            </div>
+            <div className="list-happend-item">
+              <span>B BRAND</span>
+              <img src={newsImg03} alt="" />
+            </div>
+            <div className="list-happend-item">
+              <span>B BRAND</span>
+              <img src={newsImg03} alt="" />
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className="section">
+        <p className="section-title">what happened</p>
+        <p className="section-content">How to create mobile-optimized videos in minutes. Not a designer,
+          every team makes a lot of videos Can be trimmed. Take the first </p>
+        <button className="brand-btn bottom">
+          SEE MORE
+        </button>
+        <div className="video">
+          <img src={video} alt="" />
+        </div>
+        <img className="videobox" src={videobox} alt="" />
+        <img className="videoObject07" src={videoObject07} alt="" />
+        <img className="tim" src={tim} alt="" />
+        <div className="objvideo">
+          <img src={objvideo} alt="" />
+        </div>
+        <img className="img-user" src={nguoi} alt="" />
+        <img className="videoObject03" src={videoObject03} alt="" />
+        <img className="error" src={error} alt="" />
+      </div>
+       <div className="footer">
+        <div className="container">
+          <div className="row_top">
+            <div className="top-left">
+              <h3>what happend</h3>
+              <ul>
+                <li>[공지] 개인 정보 처리 방침 변경 사전 안내</li>
+                <li>[공지] 개인 정보 처리 방침 변경 사전 안내</li>
+                <li>[공지] 개인 정보 처리 방침 변경 사전 안내</li>
+                <li>[공지] 개인 정보 처리 방침 변경 사전 안내</li>
+                <li>[공지] 개인 정보 처리 방침 변경 사전 안내</li>
+              </ul>
+            </div>
+            <div className="top-right">
+              <div className="list-info">
+                <h3>about us</h3>
+                <ul className="menu">
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                </ul>
+              </div>
+              <div className="list-info">
+                <h3>my order</h3>
+                <ul className="menu">
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                </ul>
+              </div>
+              <div className="list-info">
+                <h3>my account</h3>
+                <ul className="menu">
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                </ul>
+              </div>
+              <div className="list-info">
+                <h3>help</h3>
+                <ul className="menu">
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                  <li>회사 소</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="row_bottom">
+            <div className="bottom-left">
+              <p>© 2020-2021 what happened corp  l  (주) 왓헤픈 ｜ 대표자 : 홍길동 ｜ 사업자 등록번호 : 356-00-00000 ㅣ test0101@what happened.co.kr</p>
+              <p>
+                서울특별시 강남구 도산대로 8길 17 ｜ FAX : 070-0000-0000  l  서비스 이용약관  l  개인정보처리방침
+              </p>
+            </div>
+              <ul className="bottom-right">
+                <li><img src={n} alt="" /></li>
+                <li><img src={n} alt="" /></li>
+                <li><img src={n} alt="" /></li>
+              </ul>
+          </div>
+        </div>
+      </div>  
+    </div>
+  );
+}
+
+export default App;
